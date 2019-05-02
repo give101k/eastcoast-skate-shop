61947e274183558540584bc59c1b1520fdd3dd73
Brandon Hanlon <give101k@gmail.com>
2019-05-02 18:36:18 -0400

    update nav bar

--- view/client.php
+++ view/client.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -23,6 +24,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="index.php?action=products">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=cart">Cart <i class="fas fa-shopping-cart"></i> <span class="label label-primary"><?php echo $_SESSION['cartqt'];?></span></a>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0" action="index.php">
--- view/displayproducts.php
+++ view/displayproducts.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/productdisplay.css">
     <script type="text/javascript" src="javascript/products.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -19,11 +20,14 @@
 
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
           <a class="nav-link" href="index.php?action=home">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item active">
           <a class="nav-link" href="index.php?action=products">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?action=cart">Cart <i class="fas fa-shopping-cart"></i> <span class="label label-primary"><?php echo $_SESSION['cartqt'];?></span></a>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0" action=".">
