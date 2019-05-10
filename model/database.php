15d3a8066b1fd141b9c35b174481cbfd3fa35b3c
Brandon Hanlon <give101k@gmail.com>
2019-05-10 19:39:29 -0400

    added error messages

--- model/database.php
+++ model/database.php
@@ -6,6 +6,7 @@ $password = 'pass';
 try {
   $db = new PDO($dsn, $username, $password);
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   $error_message = $e->getMessage();
   include 'database_error.php';
